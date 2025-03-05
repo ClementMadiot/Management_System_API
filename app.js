250c24609f299821ef33c75e213f4e892f778103
ClementMadiot <clementmadiot09@gmail.com>
2025-03-05 13:21:34 +0100

    connect to MongoDB

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './databases/mongodb.js';
 
 
 const app = express();
@@ -18,9 +19,9 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
-  
+  await connectToDatabase();
 });
 
 export default app;
